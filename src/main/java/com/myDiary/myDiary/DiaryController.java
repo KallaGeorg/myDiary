0be79a0f5337aec7a1874faca76e0a38f03fb924
KallaGeorg <kalla.georg@yahoo.com>
2023-12-24 14:26:11 +0100

    done

--- src/main/java/com/myDiary/myDiary/DiaryController.java
+++ src/main/java/com/myDiary/myDiary/DiaryController.java
@@ -8,8 +8,10 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class DiaryController {
@@ -46,6 +48,49 @@ public class DiaryController {
     diaryRepository.deleteById(id);
     return"redirect:/diary";
     } 
+
+    @GetMapping("/editForm")
+    public String editPage(Model model){
+        model.addAttribute("allNotes",diaryRepository.findAll());
+        return"redirect:/diary";
+
+    }
+    @GetMapping("/editForm/{id}")
+    public String editForm(@PathVariable int id, Model model){
+        DiaryNote diarynote = diaryRepository.findById(id).orElse(null);
+        model.addAttribute("diarynote", diarynote);
+        return"editForm";
+    }
+  
+    @PostMapping("/editForm/{id}")
+    public String editNote(@PathVariable int id,
+                            @RequestParam("date") String editDatetime,  
+                            @RequestParam("headline")String editHeadline,
+                            @RequestParam("note")String editNote){
+    DiaryNote currentDiaryNote = diaryRepository.findById(id).orElse(null);
+    if(currentDiaryNote != null){
+    currentDiaryNote.setDate(LocalDateTime.parse(editDatetime));
+    currentDiaryNote.setHeadline(editHeadline);
+    currentDiaryNote.setNote(editNote);
+                  diaryRepository.save(currentDiaryNote);     
+    }
+    return"redirect:/editForm";
+  }
+     
+  @GetMapping("/search")
+   public String searchForm(){
+    return"search";
+}
+
+    @GetMapping("/runSearch")
+   public String showNotesBetween(Model model,@RequestParam("startDateTime") LocalDateTime startDateTime,
+                                             @RequestParam("endDateTime")LocalDateTime endDateTime,
+                                             RedirectAttributes redirectAttributes){
+     List<DiaryNote> notesBetween1 = diaryRepository.notesBetween(startDateTime,endDateTime);
+     redirectAttributes.addFlashAttribute("notesBetween1", notesBetween1);
+     return"redirect:/search";
+    }
+      
 
    }
 
